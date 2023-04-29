28bad63d71da8ce1c76c07e70dbdf1d59f265ee4
Logan <10239411+Loag@users.noreply.github.com>
2023-04-28 22:34:22 -0400

    use feature instead of script

--- .devcontainer/install-elan.sh
+++
@@ -1,9 +0,0 @@
-cd ~/
-
-tar -xzf elan-x86_64-unknown-linux-gnu.tar.gz && ./elan-init -y
-
-echo -n 'export PATH="/home/vscode/.elan/bin:${PATH}"' >> ~/.zshrc
-
-export PATH="/home/vscode/.elan/bin:${PATH}"
-
-elan self update && elan default leanprover/lean4:stable
